9677fff66300636907983f59e64c35cda1106e33
U-BSD\462750 <462750@SHS-H046ND2.beaverton.k12.or.us>
2022-09-26 12:00:25 -0700

    fix the board

--- tic.cpp
+++ tic.cpp
@@ -13,8 +13,12 @@ using namespace std;
 int main() {
 
   bool XTurn = true;
-  char Board[4][4] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};// '10', '11', '12', '13', '14', '15'};
-  cout << Board << endl;
+  char Board[4][4] = {{'1','2','3','4'}, {'1','2','3','3'},{'1','2','3','4'}, {'1','2','3','3'}};
+  //cout << Board << endl;
+  for (int i = 0; i < 4; i++) {
+    cout << Board[i] << "/n";
+
+  }
 }
 
 void CheckWin () {
