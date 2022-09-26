31864ba1102e3be6e8a19baf4a0119b24e7d6c15
U-BSD\462750 <462750@SHS-H046ND2.beaverton.k12.or.us>
2022-09-26 11:42:14 -0700

    test push

--- tic.cpp
+++ tic.cpp
@@ -1,1 +1,30 @@
 // no code but make sure you use letters and numbers to determine spaces
+/*
+Tej Hiremath
+This program allows the user to play TicTacToe in the terminal window.
+date
+*/
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main() {
+
+  bool XTurn = true;
+  char Board[4][4] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};// '10', '11', '12', '13', '14', '15'};
+  cout << Board << endl;
+}
+
+void CheckWin () {
+
+
+}
+
+
+void CheckLegal () {
+
+
+
+}
