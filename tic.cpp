abb3a3789bcadaf227dd82ba75f53756302eb303
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-09-27 12:23:07 -0700

    A lot to fix, but after that you only have to add game over, score, and check for win every turn

--- tic.cpp
+++ tic.cpp
@@ -1,34 +1,132 @@
-// no code but make sure you use letters and numbers to determine spaces
 /*
 Tej Hiremath
 This program allows the user to play TicTacToe in the terminal window.
 date
 */
-#include <iostream>
-#include <cstring>
 
+#include<iostream>
 using namespace std;
 
+void printBoard() {
+  int i,j;
+  //cout<<"Printing a 2D Array:\n";
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      cout<</*"\t"<<*/Board[i][j];
+    }
+    cout<<endl;
+  }
+  Turn();
+}
 
-int main() {
-
+void Turn( ) {
   bool XTurn = true;
-  char Board[4][4] = {{'1','2','3','4'}, {'1','2','3','3'},{'1','2','3','4'}, {'1','2','3','3'}};
-  //cout << Board << endl;
-  for (int i = 0; i < 4; i++) {
-    cout << Board[i] << "/n";
+  bool EndGame = false;
+  bool ValidTurn = false;
+  bool Print;
+  char Column, Row;
+  do {
+    if (XTurn == true) {
+      cout << "It is the X players turn." << endl;
+      do {
+	cout << "Enter what column you would like your input to go in." << endl;
+	cin >> Column;
+	if (Column != '1', '2', '3') {
+	  cout << "Not a valid input." << endl;
+	}
+	else {
+	  cout << "Enter what row you would like your input to go in. ('a', 'b', 'c')" << endl;
+	  cin >> Row;
+	  if (Row != 'a', 'b', 'c') {
+	    cout << "Not a valid input." << endl;
+	  }
+	  else {
+	    if (Column == '1') {
+	     j = 2;
+	    }
+	    else if (Column == '2') {
+	      j = 3;
+	    }
+	    else {
+	      j = 4;
+	    }
+	    if (Row == 'a') {
+	      i = 2;
+	    }
+	    else if (Row == 'b') {
+	      i = 3;
+	    }
+	    else {
+	      j = 4;
+	    }
+	    Board[i][j] = 'x';
+	    Printed = true;
+	    XTurn = false;
+	    ValidTurn = true; 
+	  }
+	}
+      } while (ValidTurn = false && XTurn = false && Printed == true)
+	Printed = false;
+    }
+    else { // O PLAYERS TURN
+      cout << "It is the O players turn." << endl;
+            do {
+	cout << "Enter what column you would like your input to go in." << endl;
+	cin >> Column;
+	if (Column != '1', '2', '3') {
+	  cout << "Not a valid input." << endl;
+	}
+	else {
+	  cout << "Enter what row you would like your input to go in. ('a', 'b', 'c')" << endl;
+	  cin >> Row;
+	  if (Row != 'a', 'b', 'c') {
+	    cout << "Not a valid input." << endl;
+	  }
+	  else {
+	    if (Column == '1') {
+	     j = 2;
+	    }
+	    else if (Column == '2') {
+	      j = 3;
+	    }
+	    else {
+	      j = 4;
+	    }
+	    if (Row == 'a') {
+	      i = 2;
+	    }
+	    else if (Row == 'b') {
+	      i = 3;
+	    }
+	    else {
+	      j = 4;
+	    }
+	    Board[i][j] = 'o';
+	    Printed = true;
+	    XTurn = true;
+	    ValidTurn = true; 
+	  }
+	}
+      } while (ValidTurn = false && XTurn = false && Printed == true)
+    }
+  } while (EndGame == false)
+    Printed = false;
+  PrintBoard();
+}
+
+main( ) {  
+
+  int XWins, OWins;
+
+  char Board[4][4] = {
+    { ' ', '1', '2', '3' },
+    { 'a', ' ', ' ', ' ' },
+    { 'b', ' ', ' ', ' ' },
+    { 'c', ' ', ' ', ' ' }
+     } ;
 
   }
-}
-
-void CheckWin () {
-
-
+  Turn();
 }
 
 
-void CheckLegal () {
-
-
-
-}
